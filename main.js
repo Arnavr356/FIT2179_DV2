81bfd39d2298b7322bd6744e1bd527e5f71ba742
Arnavr356 <arnavr356@gmail.com>
2026-05-22 00:05:02 +1000

    Added webpage layout and species bar chart

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 vegaEmbed("#map", "js/map.json");
 
-vegaEmbed("#species_bar", "js/species_bar.vg.json");
+vegaEmbed("#species_bar", "js/species_bar.json");
 
 vegaEmbed("#heatmap", "js/heatmap.vg.json");
