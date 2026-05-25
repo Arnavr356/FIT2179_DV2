05806cd0b2ec21e51f5775f3c55435a0a53b30ce
Arnavr356 <arnavr356@gmail.com>
2026-05-25 13:28:00 +1000

    Design changes

--- main.js
+++
@@ -1,8 +0,0 @@
-vegaEmbed("#map", "js/map.json")
-.catch(console.error);
-
-vegaEmbed("#species_bar", "js/species_bar.json")
-.catch(console.error);
-
-vegaEmbed("#heatmap", "js/heatmap.json")
-.catch(console.error);
